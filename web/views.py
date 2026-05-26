f87176766ec95bfceac786cc46403937ecea0247
rainmartaniamarrosuli <rainmartaniamrosuli@gmail.com>
2026-05-26 19:41:37 +0700

    belum selesai

--- web/views.py
+++ web/views.py
@@ -3,19 +3,15 @@ from django.http import JsonResponse
 from .models import Mahasiswa, Berita, Jurusan
 
 def beranda(request):
-    # Mengambil semua data berita, diurutkan dari yang terbaru
     berita_list = Berita.objects.all().order_by('-tanggal_dibuat')
     
-    # Memasukkan data ke dalam dictionary 'context'
     context = {
         'berita_list': berita_list
     }
     
-    # Mengirim context ke template
     return render(request, 'index.html', context)
 
 def mahasiswa_json(request):
-    # Mengambil semua data mahasiswa dari database
     mahasiswa_list = Mahasiswa.objects.all()
     
     data = []
@@ -25,8 +21,7 @@ def mahasiswa_json(request):
             'nim': mhs.nim,
             'nama': mhs.nama,
             'email': mhs.email,
-            'id_jur': mhs.id_jur.nama if mhs.id_jur else '-'  # Menampilkan nama jurusan
+            'id_jur': mhs.id_jur.nama if mhs.id_jur else '-'  
         })
         
-    # Mengembalikan data dalam format JSON yang dimengerti DataTables
     return JsonResponse({'data': data})
